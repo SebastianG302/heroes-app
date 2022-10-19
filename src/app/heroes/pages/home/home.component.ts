cbce2c7cc5e3e9fff39406f4d27cb626cf2675f2
SebastianG302 <johan.guerrero@pi.edu.co>
2022-10-19 01:51:12 -0500

    sidenav update

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -3,7 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
+  styles: [`
+  .sidenav-container{
+    max-width: 2000px;
+    margin: 0 auto;
+    width: 100%;
+    display:flex
+  }
+    .container{
+      margin: 10px;
+    }
+  `
   ]
 })
 export class HomeComponent implements OnInit {
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,14 +4,15 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
-
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   exports: [
     MatSidenavModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule
   ]
 
 })
