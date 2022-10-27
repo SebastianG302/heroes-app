888f9a91046026ad3de7ba5442b637184a2fb1f2
SebastianG302 <johan.guerrero@pi.edu.co>
2022-10-26 23:05:42 -0500

    heroes component update

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
@@ -8,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private route: ActivatedRoute) {
+    
+  }
+
+  
 
   ngOnInit(): void {
+    const id = this.route.snapshot.params['id'];
+    console.log(id);
+    
+    
   }
 
 }
