4b85b2903a794b647f733462702457fdb4bbc78c
SebastianG302 <johan.guerrero@pi.edu.co>
2022-11-01 23:43:46 -0500

    heroe component update

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, switchMap } from 'rxjs';
 import { Heroe } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
-  ]
+  styles: [`
+    img{
+      width: 100%;
+      border-radius: 5px;
+    }
+  `]
 })
 export class HeroeComponent implements OnInit {
 
   heroe! : Heroe;
-  constructor(private route: ActivatedRoute, private heroesService: HeroesService) {
+  constructor(
+    private route: ActivatedRoute,
+    private heroesService: HeroesService,
+    private router: Router) {
     
   }
 
@@ -23,8 +30,9 @@ export class HeroeComponent implements OnInit {
       switchMap(({id}) => this.heroesService.getHeroesId(id))
     )
     .subscribe( heroe => this.heroe = heroe);
-    
-
   }
 
+  goBack(){
+    this.router.navigate(['/heroes/list'])
+  }
 }
